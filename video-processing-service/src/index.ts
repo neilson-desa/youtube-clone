411820dee288f9db2a910889cb9d22c5146b87f3
neilson-desa <neilsondesa@hotmail.co.uk>
2024-06-07 16:20:36 +0100

    added post endpoint for uploading and converting a video to 360p + containerized the application

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,14 +1,32 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-// create get endpoint
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// create post endpoint
+app.post('/process-video', (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send('Bad Request: Missing input or output file path.');
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360")
+        .on("end", () => {
+            res.status(200).send('Video processing finished successfully.');
+        })
+        .on("error", (err) => {
+            console.log(`An error occurred:: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`);
+        })
+        .save(outputFilePath);
 })
 
 // start express server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`server started at http://localhost:${port}`);
 })
