6d2918fb298e33fcb3ef41f8dc50e3d8ffff256b
neilson-desa <neilsondesa@hotmail.co.uk>
2025-02-19 13:58:31 +0000

    created firebase function that listens for new user sign ins (which trigger auth.user().onCreate() event) that then does a callback which adds a new user documents to our project's firestore db

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -7,13 +7,27 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import * as auth from "firebase-functions/v1/auth";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+initializeApp();
+
+const firestore = new Firestore();
+
+// triggered by signInWithGoogle function in web client (see react code)
+export const createUser = auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  firestore.collection("users").doc(userInfo.uid).set(userInfo);
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+  return;
 });
