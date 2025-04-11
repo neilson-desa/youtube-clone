db34fa9860834ac5e939225214b328150a042fe3
neilson-desa <neilsondesa@hotmail.co.uk>
2025-04-11 17:45:16 +0100

    Adding spring boot project to migrate video processing service over to

---
+++ video-processing-spring-boot-service/src/main/java/com/neilsondesa/video_processing_spring_boot_service/VideoProcessingSpringBootServiceApplication.java
@@ -0,0 +1,13 @@
+package com.neilsondesa.video_processing_spring_boot_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoProcessingSpringBootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoProcessingSpringBootServiceApplication.class, args);
+	}
+
+}
---
+++ video-processing-spring-boot-service/src/test/java/com/neilsondesa/video_processing_spring_boot_service/VideoProcessingSpringBootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neilsondesa.video_processing_spring_boot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoProcessingSpringBootServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
